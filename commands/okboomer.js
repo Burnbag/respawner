ce790afc2739b0a169f530a3acc2131e23315033
Burnbag <ian.palisak@gmail.com>
2019-12-07 14:25:02 +0100

    boomer cmd only with VIP role

--- commands/okboomer.js
+++ commands/okboomer.js
@@ -2,15 +2,20 @@ const Discord = require("discord.js");
 const config = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
-        
+    
+    let modrole = message.guild.roles.find("name", "VIP");
+    if (!message.member.roles.has(modrole.id)) {
+      return message.reply("Nemaš dozvolu za ovu komandu !");
+    }
     var timesRun = 0;
+    var boomerCmdRunNumber = 5;
     var interval = setInterval(function(){
         timesRun += 1;
-        if(timesRun === 5){
+        if(timesRun === boomerCmdRunNumber){
             clearInterval(interval);
         }
         message.channel.sendMessage("Ok boomer!");
-    }, 1); 
+    }, 500); 
 }
 
 module.exports.help = {
